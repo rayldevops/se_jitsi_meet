705a550b90d5d77fc8f1a153470fefecc04bfe53
RAYL <devops@rayl.dev>
2022-05-11 20:26:09 +0530

    Shifted Lines

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Video Call/Conference with Jitsi Meet. Integration with Odoo',
+    'name': 'Video Call/Conference with Rayl Meet. Integration with Rayl',
     'version': '13.0.1.0.0',
     'category': 'Extra Tools',
-    'website': "https://softwareescarlata.com/",
+    'website': "https://rayl.com",
     'sequence': 1,
-    'summary': 'Create and share Jitsi Meet video conferences with other users',
+    'summary': 'Create and share Rayl Meet video conferences with other users',
     'description': """
-		Adds a new APP to create and share Jisti Meet video conference meetings between Odoo users.
-		When you join the meeting Odoo opens a new browser tab so you can keep working on Odoo, and share screen with your partners at Jisti Meet. 
+		Adds a new APP to create and share Rayl Meet video conference meetings between Rayl users.
+		When you join the meeting Rayl opens a new browser tab so you can keep working on Rayl, and share screen with your partners at Rayl Meet. 
     """,
-    'author': "David Montero Crespo",
+    'author': "Rayl",
     "depends": ['base', 'web', 'mail', 'website'],
+    'pre_init_hook': '_install_required_package',
     "data": [
         'views/se_jitsi_meet_views.xml',
         'views/template.xml',
@@ -19,10 +20,8 @@
         'data/mail_template.xml',
         'security/ir.model.access.csv',
         'security/base_security.xml',
-
     ],
     'images': ['static/description/0.jpg'],
-    'pre_init_hook': '_install_required_package',
     'installable': True,
     'auto_install': False,
     'application': True,
